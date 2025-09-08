20397065d2083f664371e14a5b358191ad11a6c2
AnamSadat <anamsadat3@gmail.com>
2025-09-09 00:26:54 +0700

    refactor: index with level atoms directory

---
+++ src/components/organism/index.ts
@@ -0,0 +1,12 @@
+// Auth
+export * from '@/components/organism/auth/LoginForm';
+export * from '@/components/organism/auth/RegisterForm';
+
+// Footer
+export * from '@/components/organism/footer/Footer';
+
+// Navbar
+export * from '@/components/organism/navbar/Navbar';
+
+// Sidebar
+export * from '@/components/organism/sidebar/Sidebar';
